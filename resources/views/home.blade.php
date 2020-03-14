13ad9d903b79a47173118860dfa2102786db321c
Vedant Singhania <voyager@Vedants-MacBook-Pro.local>
2020-03-14 10:32:17 -0600

    first commit,repo initilization

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+<!-- User Feed -->
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+
+        </div>
+        <div class="col-9">
+        </div>
+    </div>
+</div>
+@endsection
